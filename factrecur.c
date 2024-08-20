bfbc5d4561b233ebe7ebcd8798025dd35cefa6e0
Suman Mondal <145131739+Suman-Mondal007@users.noreply.github.com>
2024-08-21 01:36:56 +0530

    Create factrecur.c

---
+++ factrecur.c
@@ -0,0 +1,17 @@
+//Write a C program to calculate the factorial of a given number using recursion.
+#include <stdio.h>
+int factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1; 
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    int result = factorial(num);
+    printf("The factorial of %d is: %d\n", num, result);
+    return 0;
+}

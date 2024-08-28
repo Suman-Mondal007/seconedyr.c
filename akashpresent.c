1821154b8a247e732a58e056e5194ed87608fc00
Suman Mondal <145131739+Suman-Mondal007@users.noreply.github.com>
2024-08-28 23:53:49 +0530

    Create akashpresent.c

---
+++ akashpresent.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int roll;
+    printf("Is it a sunny day. \n");
+    printf("The teacher arrived. \n");
+            printf("Enter Akash roll number: ");
+            scanf(" %d", &roll);
+            if (roll == 131) {
+                printf("Akash is present in the class.\n");
+            } else {
+                printf("Akash is not present in the class.\n");
+            }
+}
